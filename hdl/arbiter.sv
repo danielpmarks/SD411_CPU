d6c665defecdd5b4803c5dfe329598a1c8d94bdf
Jincheng Liu <jl81@linux-33.ews.illinois.edu>
2021-10-21 21:46:45 -0500

    added rst signal in arbiter

--- hdl/arbiter.sv
+++ hdl/arbiter.sv
@@ -1,6 +1,6 @@
 module arbiter(
     input clk,
-
+    input rst,
     //data cache
 
     input logic [31:0] pmem_address_c_d, // from llc
@@ -50,32 +50,33 @@ always_comb begin
     pmem_rdata_c_d  = 0;
 
 	case(state)
-    idle: begin
-      if (pmem_read_c_d || pmem_write_c_d) begin
-          pmem_read_m = pmem_read_c_d;
-          pmem_write_m = pmem_write_c_d;
-          pmem_address_m = pmem_address_c_d;
-          pmem_wdata_m = pmem_wdata_c_d;
-      end else if (pmem_read_c_i || pmem_write_c_i) begin
-          pmem_read_m = pmem_read_c_i;
-          pmem_write_m = pmem_write_c_i;
-          pmem_address_m = pmem_address_c_i;
-          pmem_wdata_m = pmem_wdata_c_i;
-      end
-    end
+        idle: begin
+            if (pmem_read_c_d || pmem_write_c_d) begin
+                pmem_read_m = pmem_read_c_d;
+                pmem_write_m = pmem_write_c_d;
+                pmem_address_m = pmem_address_c_d;
+                pmem_wdata_m = pmem_wdata_c_d;
+            end else if (pmem_read_c_i || pmem_write_c_i) begin
+                pmem_read_m = pmem_read_c_i;
+                pmem_write_m = pmem_write_c_i;
+                pmem_address_m = pmem_address_c_i;
+                pmem_wdata_m = pmem_wdata_c_i;
+            end
+        end
 
-    instruction_cache: begin
-      if (pmem_resp_m) begin
-        pmem_resp_c_i = pmem_resp_m;
-        pmem_rdata_c_i = pmem_rdata_m;
-      end
-    end
-    data_cache: begin
-      if (pmem_resp_m) begin
-        pmem_resp_c_d = pmem_resp_m;
-        pmem_rdata_c_d = pmem_rdata_m;
-      end
-    end
+        instruction_cache: begin
+            if (pmem_resp_m) begin
+                pmem_resp_c_i = pmem_resp_m;
+                pmem_rdata_c_i = pmem_rdata_m;
+            end
+        end
+
+        data_cache: begin
+            if (pmem_resp_m) begin
+                pmem_resp_c_d = pmem_resp_m;
+                pmem_rdata_c_d = pmem_rdata_m;
+            end
+        end
 	endcase
 end
 
@@ -86,30 +87,36 @@ always_comb begin : next_state_logic
 	next_state = state;
 
 	case(state)
-    idle: begin
-      if (pmem_read_c_i || pmem_write_c_i) begin
-          next_state = instruction_cache;
-      end else if (pmem_read_c_d || pmem_write_c_d) begin
-          next_state = data_cache;
-      end
+        idle: begin
+            if (pmem_read_c_i || pmem_write_c_i) begin
+                next_state = instruction_cache;
+            end else if (pmem_read_c_d || pmem_write_c_d) begin
+                next_state = data_cache;
+            end
 
-    end
-
-    instruction_cache: begin
-      if (pmem_resp_m) begin
-          next_state = idle;
-      end
-    end
-    data_cache: begin
-        if (pmem_resp_m) begin
-            next_state = idle;
         end
-    end
+
+        instruction_cache: begin
+            if (pmem_resp_m) begin
+                next_state = idle;
+            end
+        end
+
+        data_cache: begin
+            if (pmem_resp_m) begin
+                next_state = idle;
+            end
+        end
 	endcase
 end
 
 always_ff @(posedge clk) begin: next_state_assignment
-	 state <= next_state;
+    if (rst) begin
+        state <= idle;
+    end else begin
+        state <= next_state;
+    end
+	 
 end
 
 endmodule : arbiter
